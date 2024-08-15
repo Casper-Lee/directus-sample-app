da9d3388d2aca4015c0f7baf183359fcd506ca1a
Casper-Lee <leecasper2000@gmail.com>
2024-08-15 10:17:00 +0800

    first commit

---
+++ client/lib/directus.js
@@ -0,0 +1,8 @@
+import { createDirectus, rest } from "@directus/sdk";
+
+const directus = createDirectus(
+  "http://0.0.0.0:8055/"
+).with(rest());
+
+console.log("directus", directus);
+export default directus;
