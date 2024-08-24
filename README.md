322f10566f0efc6e1843d8f4991ef33662843d6f
Casper-Lee <leecasper2000@gmail.com>
2024-08-24 14:46:06 +0800

    feat: add new pages and navigation feat: create pages to display the store the content and title feat: create posts to display the blog feat: include navigation fix: remove .babelrc fix: remove development.dockerfile and remove client from docker-compose.yml

--- README.md
+++ README.md
@@ -10,8 +10,11 @@ Step 1: 🚀 Initial Setup
 Step 2: 🏃‍♂️ Running the Application
 
 * Run `docker-compose up --build`
+* Run `cd client && npm run dev`
 
 ## References
 [Build a Website With Next.js and the Directus JavaScript SDK](https://docs.directus.io/guides/headless-cms/build-static-website/next.html)
+
+[Get Started with Directus and Next.js](https://directus.io/tv/stack-up/next)
 
 [Self-Hosting Quickstart](https://docs.directus.io/self-hosted/quickstart.html)
---
+++ client/app/[slug]/page.jsx
@@ -0,0 +1,18 @@
+import directus from "@/lib/directus";
+import { notFound } from "next/navigation";
+import { readItem } from "@directus/sdk";
+
+async function getPage(slug) {
+  const page = await directus.request(readItem("pages", slug));
+  return page;
+}
+
+export default async function DynamicPage({ params }) {
+  const page = await getPage(params.slug);
+  return (
+    <div>
+      <h1>{page.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: page.content }}></div>
+    </div>
+  );
+}
